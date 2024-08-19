2ecf0fb84cf55b3b799a8bdab2cd9d96c62e502b
Cas74l4n <63763425+Cas74l4n@users.noreply.github.com>
2024-08-19 03:43:52 -0600

    Refactorizacion y Mas arreglos 3

--- src/components/React.svelte
+++ src/components/React.svelte
@@ -45,7 +45,7 @@
       class="py-2 my-2.5 px-4 flex justify-center items-center bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
       >
       <Upload />
-      Fix my English!
+      Fix My English!
     </button>
   {:else}
     {#await promise}
@@ -76,5 +76,3 @@
     {/if}
   </div>
 </div>
-
-
