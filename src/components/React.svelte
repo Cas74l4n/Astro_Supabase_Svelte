c4a767cf0c23c30e72b12598a459a2bbcc75f505
Cas74l4n <63763425+Cas74l4n@users.noreply.github.com>
2024-08-19 02:15:25 -0600

    Refactorizacion y Mas arreglos 2

--- src/components/React.svelte
+++ src/components/React.svelte
@@ -10,7 +10,7 @@
 
   const handleClick = async () => {
     inputText = document.getElementById("result").value.trim();
-    if (inputText === "") return; 
+    if (inputText === "") return;
     promise = fixMyEnglish(inputText);
     const value = await promise;
 
@@ -43,7 +43,6 @@
       on:click={handleClick}
       type="button"
       class="py-2 my-2.5 px-4 flex justify-center items-center bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
-      disabled={inputText === ""}
       >
       <Upload />
       Fix my English!
