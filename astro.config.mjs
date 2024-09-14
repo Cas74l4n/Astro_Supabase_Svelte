c9b552dcbb3777b61e6e9a757b9c93a5f886db2a
Cas74l4n <63763425+Cas74l4n@users.noreply.github.com>
2024-09-14 02:10:21 -0600

    intento 2 Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel/serverless";
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
+  site: "https://astro-supabase-svelte.vercel.app",
   output: 'server',
-  adapter: vercel(),  // Cambia a vercel()
+  adapter: vercel(),
   integrations: [svelte(), tailwind()],
 });
