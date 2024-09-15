a9051632057e05d4e04fd5d454dfc2fd60253e81
Cas74l4n <63763425+Cas74l4n@users.noreply.github.com>
2024-09-15 15:49:00 -0600

    Deploy Local

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),  // Cambia a vercel()
   integrations: [svelte(), tailwind()],
 });
