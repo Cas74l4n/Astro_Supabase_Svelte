318ebe4d070dc1b38f663a7544839bcb0685ac26
Cas74l4n <castelan.15@outlook.es>
2024-10-29 16:54:41 -0600

    Modificacion de signin y callback

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -5,12 +5,16 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
   const authCode = url.searchParams.get("code");
 
   if (!authCode) {
+    console.log("No se recibió ningún código en el callback");
     return new Response("No se proporcionó ningún código", { status: 400 });
   }
+
+  console.log("Código de autenticación recibido:", authCode);
 
   const { data, error } = await supabase.auth.exchangeCodeForSession(authCode);
 
   if (error) {
+    console.error("Error al intercambiar el código por una sesión:", error.message);
     return new Response(error.message, { status: 500 });
   }
 
@@ -26,5 +30,6 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
     secure: true,
     httpOnly: true,
   });
+
   return redirect("/corrector");
 };
