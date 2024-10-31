20a40daf7f40eddc0ff7eb9c1b61585cb4ea921f
Cas74l4n <castelan.15@outlook.es>
2024-10-31 00:43:59 -0600

    {scope:'global',} en sigout.ts

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -3,7 +3,7 @@ import { supabase } from "../../../lib/supabase";
 
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
-  const { error } = await supabase.auth.signOut()
+  const { error } = await supabase.auth.signOut({scope:'global',})
  /*  cookies.delete("sb-access-token", { path: "/" });
   cookies.delete("sb-refresh-token", { path: "/" }); */
   return redirect("/signin");
