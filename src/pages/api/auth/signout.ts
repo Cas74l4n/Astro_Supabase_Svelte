c857def8c6242e621e95e0bd2d8cf42a21df7d2d
Cas74l4n <castelan.15@outlook.es>
2024-10-31 00:32:19 -0600

    fix oauth

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -3,7 +3,7 @@ import { supabase } from "../../../lib/supabase";
 
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
-  const { error } = await supabase.auth.signOut({scope:'global',})
+  const { error } = await supabase.auth.signOut()
  /*  cookies.delete("sb-access-token", { path: "/" });
   cookies.delete("sb-refresh-token", { path: "/" }); */
   return redirect("/signin");
