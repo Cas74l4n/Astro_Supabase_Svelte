53ae18692861153ca4d80932f7bba8bf2d5ce781
Cas74l4n <castelan.15@outlook.es>
2024-11-04 20:18:33 -0600

    Amplify integrado.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,11 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
+import amplify from 'astro-aws-amplify';
+
 
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   output: 'server',
-  adapter: netlify(),
-  vite: {
-    resolve: {
-      alias: {
-        // Configuración para manejar módulos de Node en Vite
-        fs: 'memfs',
-        path: 'path-browserify',
-      },
-    },
-  },
+  adapter: amplify()
 });
